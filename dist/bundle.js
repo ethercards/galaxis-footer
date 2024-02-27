7977b9d1e5b78ab24fbde1bf8101f1c2801cc4e7
kovendilevi <kovendilevi@camelcoding.com>
2024-02-27 16:23:37 +0200

    I create another variant

---
+++ dist/bundle.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var React = require('react');
+
+var MyComponent = function (_a) {
+    var text = _a.text;
+    return (React.createElement("div", null, text));
+};
+
+exports.MyComponent = MyComponent;
+//# sourceMappingURL=bundle.js.map
--- src/components/Footer/Footer.tsx
+++
@@ -1,7 +0,0 @@
-import React, { FC } from "react";
-
-const Footer: FC = () => {
-  return (<div>Hello</div>)
-};
-
-export default Footer;

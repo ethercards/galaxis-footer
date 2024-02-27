5a472eb8e964ab969620b0c3c31059a09d1aef21
kovendilevi <kovendilevi@camelcoding.com>
2024-02-27 13:08:25 +0200

    Update the package.json file

---
+++ dist/GalaxisFooter.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+interface GalaxisFooterProps {
+}
+declare const GalaxisFooter: React.FC<GalaxisFooterProps>;
+export default GalaxisFooter;
---
+++ dist/index.module.js
@@ -0,0 +1,2 @@
+import{jsx as r}from"react/jsx-runtime";import e from"react";import t from"react-dom/client";const o=()=>r("div",{children:"Hello from GalaxisFooter!"});t.createRoot(document.getElementById("root")).render(r(e.StrictMode,{children:r(function(){return r("div",{className:"App",children:r(o,{})})},{})}));
+//# sourceMappingURL=index.module.js.map

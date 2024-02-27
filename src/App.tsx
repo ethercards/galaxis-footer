868b940bc7c46b07791e35d0e4f93c0835040e55
kovendilevi <kovendilevi@camelcoding.com>
2024-02-27 12:52:58 +0200

    Start commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import GalaxisFooter from './GalaxisFooter';
+
+function App() {
+  return (
+    <div className="App">
+      <GalaxisFooter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/GalaxisFooter.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface GalaxisFooterProps {
+  // Define your component props here
+}
+
+const GalaxisFooter: React.FC<GalaxisFooterProps> = () => {
+  return <div>Hello from GalaxisFooter!</div>;
+};
+
+export default GalaxisFooter;

8c3e09d24c599a31e82b77e9d7bd5bca5a29b95b
EmokeErdely <108658032+EmokeErdely@users.noreply.github.com>
2024-02-29 13:32:15 +0200

    map function simplified

--- src/mappers/cms-model.mapper.ts
+++ src/mappers/cms-model.mapper.ts
@@ -3,28 +3,22 @@ import { CmsModel } from '../models/cms.model';
 
 export const toModel = (dto: CmsDTO): CmsModel => ({
   aboutUs: dto.about_us,
-  pages: dto.pages.map((page) => {
-    return {
-      label: page.label,
-      url: page.url,
-      openInNewTab: page.open_in_new_tab,
-    };
-  }),
-  socialMediaIcons: dto.social_media_icons.map((icon) => {
-    return {
-      iconPath: icon.icon_path,
-      url: icon.url,
-    };
-  }),
+  pages: dto.pages.map((page) => ({
+    label: page.label,
+    url: page.url,
+    openInNewTab: page.open_in_new_tab,
+  })),
+  socialMediaIcons: dto.social_media_icons.map((icon) => ({
+    iconPath: icon.icon_path,
+    url: icon.url,
+  })),
   address: {
     mailingAddress: dto.address.mailing_address,
     emailAddress: dto.address.email_address,
   },
-  popular: dto.popular.map((popularItem) => {
-    return {
-      label: popularItem.label,
-      url: popularItem.url,
-      openInNewTab: popularItem.open_in_new_tab,
-    };
-  }),
+  popular: dto.popular.map((popularItem) => ({
+    label: popularItem.label,
+    url: popularItem.url,
+    openInNewTab: popularItem.open_in_new_tab,
+  })),
 });

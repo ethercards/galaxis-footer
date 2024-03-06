5c64b51150863f3214088670a05f55df4ecc9d3a
kovendilevi <kovendilevi@camelcoding.com>
2024-03-06 15:34:18 +0200

    I added the webpack config

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 8192,
+            name: "fonts/[name].[ext]",
+          },
+        },
+      },
+    ],
+  },
+};

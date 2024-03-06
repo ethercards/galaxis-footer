21bebeeb380bfc61677633ca281b3928ccabbe93
szidieross <szidonia@camelcoding.com>
2024-03-06 11:24:56 +0200

    1154: handle the links in the footer

--- src/galaxis-footer/pages/pages.component.tsx
+++ src/galaxis-footer/pages/pages.component.tsx
@@ -1,19 +1,38 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { PageModel } from "../../models/page.model";
-import { Box, Typography } from "@mui/material";
+import { Box, Link, Typography } from "@mui/material";
 import CustomTitle from "../title/title.component";
 import StyledWrapper from "./pages.style";
 
 type PagesProps = { pages: PageModel[] };
 
-const Pages: FC<PagesProps> = ({ pages }) => {
+const Pages: FC<PagesProps> = ({ pages: initialPages }) => {
+  const [pages, setPages] = useState<PageModel[]>(initialPages);
+
+  useEffect(() => {
+    const path = window.location.pathname;
+    if (path.includes("engines")) {
+      const updatedPages = initialPages.map((item) => ({
+        ...item,
+        openInNewTab: true,
+      }));
+      setPages(updatedPages);
+    }
+  }, [initialPages]);
+
   return (
     <StyledWrapper>
       <CustomTitle className="title" title="Pages" />
       <Box className="pages--box" data-testid="pages-box">
         {pages.map((page, index) => (
           <Typography className="pages--typography" key={index} variant="main">
-            <a>{page.label}</a>
+            <Link
+              className="pages--link"
+              href={page.url}
+              target={page.openInNewTab ? "_blank" : "_self"}
+            >
+              {page.label}
+            </Link>
           </Typography>
         ))}
       </Box>
--- src/galaxis-footer/popular/popular.component.tsx
+++ src/galaxis-footer/popular/popular.component.tsx
@@ -1,12 +1,25 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import StyledWrapper from "./popular.style";
 import CustomTitle from "../title/title.component";
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Link, Typography } from "@mui/material";
 import { PopularModel } from "../../models/popular.model";
 
 type PopularsProps = { popular: PopularModel[] };
 
-const Popular: FC<PopularsProps> = ({ popular }) => {
+const Popular: FC<PopularsProps> = ({ popular: initialPopular }) => {
+  const [popular, setPopular] = useState<PopularModel[]>(initialPopular);
+
+  useEffect(() => {
+    const path = window.location.pathname;
+    if (path.includes("engines")) {
+      const updatedPopular = initialPopular.map((item) => ({
+        ...item,
+        openInNewTab: true,
+      }));
+      setPopular(updatedPopular);
+    }
+  }, [initialPopular]);
+
   return (
     <StyledWrapper>
       <CustomTitle className="popular--custom-title" title="Popular" />
@@ -15,7 +28,13 @@ const Popular: FC<PopularsProps> = ({ popular }) => {
           popular.map((item, index) => (
             <Button className="popular--button" key={index} variant="outlined" disableRipple>
               <Typography className="popular--typography" variant="main">
-                {item.label.toLowerCase()}
+                <Link
+                  className="popular--link"
+                  href={item.url}
+                  target={item.openInNewTab ? "_blank" : "_self"}
+                >
+                  {item.label.toLowerCase()}
+                </Link>
               </Typography>
             </Button>
           ))}

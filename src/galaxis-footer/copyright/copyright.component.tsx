c7fae2258b7b81fa357a8bda64909910a0ccbe8b
kovendilevi <kovendilevi@camelcoding.com>
2024-03-06 13:07:00 +0200

    I added the galaxis logo as svg into the icons component

--- src/galaxis-footer/copyright/copyright.component.tsx
+++ src/galaxis-footer/copyright/copyright.component.tsx
@@ -1,12 +1,14 @@
 import React, { FC } from "react";
 import { Typography } from "@mui/material";
 import StyledWrapper from "./copyright.style";
+import { GalaxisLogo } from "../../icons/icons.component";
 
 const Copyright: FC = () => {
   const currentYear = new Date().getFullYear();
 
   return (
     <StyledWrapper>
+      <GalaxisLogo />
       <Typography className="copyright--typography" variant="main">
         All Rights Reserved 2021-{currentYear}
       </Typography>
